313435d7f87a9212f425173f1dd74298a738f151
Sharony Braam <fogsliezeta@hotmail.com>
2022-05-27 18:15:41 +0200

    removed eureka

--- src/main/java/watcherz/authenticationservice/AuthenticationServiceApplication.java
+++ src/main/java/watcherz/authenticationservice/AuthenticationServiceApplication.java
@@ -2,10 +2,8 @@ package watcherz.authenticationservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class AuthenticationServiceApplication {
 
 	public static void main(String[] args) {
